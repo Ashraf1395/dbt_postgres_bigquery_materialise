9dd87d71e213cc1b5658fd4221a577219ff4cc2b
Ashraf1395 <1395ashraf@gmail.com>
2024-02-05 17:06:48 +0530

    Update from remote

--- models/staging/stg_yellow_taxi_data.sql
+++ models/staging/stg_yellow_taxi_data.sql
@@ -9,7 +9,7 @@
 with tripdata as 
 (
   select *,
-    row_number() over(partition by vendorid, lpep_pickup_datetime) as rn
+    row_number() over(partition by vendorid, tpep_pickup_datetime) as rn
   from {{ source('staging','yellow_taxi_data') }}
   where vendorid is not null 
 )
