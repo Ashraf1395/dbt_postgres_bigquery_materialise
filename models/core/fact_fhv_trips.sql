4c8c419b9af9346a3334645361f63b9c1397564b
Ashraf1395 <1395ashraf@gmail.com>
2024-02-06 16:19:00 +0530

    Update from local

---
+++ models/core/fact_fhv_trips.sql
@@ -0,0 +1,30 @@
+{{
+    config(
+        materialized='table'
+    )
+}}
+
+with fhv_tripdata as (
+    select *, 
+        'FHV' as service_type
+    from {{ ref('stg_fhv_taxi_data') }}
+), 
+dim_zones as (
+    select * from {{ ref('dimension_zones') }}
+    where borough != 'Unknown'
+)
+select 
+fhv_tripdata.vendorid	
+fhv_tripdata.ratecodeid		
+fhv_tripdata.pickup_locationid	
+fhv_tripdata.dropoff_locationid	
+fhv_tripdata.pickup_datetime	
+fhv_tripdata.dropoff_datetime	
+fhv_tripdata.store_and_fwd_flag
+
+from fhv_tripdata.
+inner join dim_zones as pickup_zone
+on fhv_tripdata.pickup_locationid = pickup_zone.locationid
+inner join dim_zones as dropoff_zone
+on fhv_tripdata.dropoff_locationid = dropoff_zone.locationid
+
